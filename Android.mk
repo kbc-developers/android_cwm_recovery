433e766e8b6b8cf6bb4639b61fb76f5a9c0aae47
sakuramilk <c.sakuramilk@gmail.com>
2014-01-15 22:18:30 +0900

    Add recovery-preprocess

--- Android.mk
+++ Android.mk
@@ -226,6 +226,14 @@ LOCAL_SRC_FILES := killrecovery.sh
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := recovery_preprocess
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := RECOVERY_EXECUTABLES
+LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/sbin
+LOCAL_SRC_FILES := recovery_preprocess
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := verifier_test.c verifier.c
 
