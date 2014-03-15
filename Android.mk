0eda71b811334cf4bafa98e2a0e5c67a3c57692c
ma34s <ma34s.sss@gmail.com>
2014-03-15 14:15:32 +0900

    Lowercasify: LGL22
    Change-Id: I9b7bf5836efe1a706c29c0783e00feb7ae4a7d34

--- Android.mk
+++ Android.mk
@@ -85,7 +85,7 @@ else ifeq ($(TARGET_PRODUCT), cm_hltekdi)
 RECOVERY_NAME := Galaxy Note3 CWM-based Recovery
 LOCAL_CFLAGS += -DTARGET_DEVICE_SCL22
 
-else ifeq ($(TARGET_PRODUCT), cm_LGL22)
+else ifeq ($(TARGET_PRODUCT), cm_lgl22)
 RECOVERY_NAME := LGL22 CWM-based Recovery
 LOCAL_CFLAGS += -DTARGET_DEVICE_LGL22
 
